b2312f0275e975e29e6b66d4c9288e6a9ad1cfed
Shinpei Watanabe <wsahtiannpaebie@gmail.com>
2024-06-18 23:46:22 +0900

    items下準備

--- lang/ja/validation.php
+++ lang/ja/validation.php
@@ -156,9 +156,9 @@ return [
     'attributes' => [
         'title' => '件名',
         'content' => '本文',
-        // 'name' => '名',
-        // 'memo' => 'メモ',
-        // 'price' => '料金',
+        'name' => '名',
+        'memo' => 'メモ',
+        'price' => '料金',
         // 'kana' => 'カナ',
         // 'tel' => '電話番号',
         // 'email' => 'メールアドレス',
